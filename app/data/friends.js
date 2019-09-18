8fa5762ff4c0c6a9050a8dcd8de51c7812a7bf5f
D Jonathan Mau <jmau@ucdavis.edu>
2019-09-18 00:50:40 -0500

    finished

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,15 +1,15 @@
 let data = [{
     name: 'Ron Weasley',
-    photo: '',
-    scores: [4,5,6,7,8,9,10,1]
+    photo: 'https://static3.srcdn.com/wordpress/wp-content/uploads/2019/04/Ron-Weasley-Defense-Against-The-Dark-Arts.jpg',
+    scores: [5,1,2,5,4,5,1,5,]
 },{
     name: 'Hermione Granger',
-    photo: '',
-    scores: [9,8,9,7,10,9,10,10]
+    photo: 'https://vignette.wikia.nocookie.net/characters/images/a/a5/Latest_%2810%29.jpg/revision/latest?cb=20141230074301',
+    scores: [1,5,3,3,4,3,5,5]
 },{
     name: 'Harry Potter',
-    photo: '',
-    scores: [2,3,1,4,5,4,3,1]
+    photo: 'https://images.pottermore.com/bxd3o8b291gf/3SQ3X2km8wkQIsQWa02yOY/25f258f21bdbe5f552a4419bb775f4f0/HarryPotter_WB_F4_HarryPotterMidshot_Promo_080615_Port.jpg?w=1200',
+    scores: [3,3,5,5,4,4,3,5]
 }]
 
 
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,14 +1,47 @@
 let data = require('../data/friends')
 
-let api = (app)=>{
-    app.get('/api/friends', (req, res)=>{
+
+let api = (app) => {
+    app.get('/api/friends', (req, res) => {
         res.json(data)
     })
-    app.post('/api/friends', (req, res)=>{
+    app.post('/api/friends', (req, res) => {
+        let bestFriendIndex = 0 
+        let totaldifference = 200
+        // for (var x = 0; x < data.length; x++) {
+        //     let difference = 0
+        //     for (var i = 0; i < req.body.scores.length; i++) {
+        //         difference += Math.abs(data[x].scores[i] - parseInt(req.body.scores[i]))
+        //     }
+        //     if (difference < totaldifference) {
+        //         totaldifference = difference;
+        //         bestFriendIndex = x
+        //     }
+        //     console.log(totaldifference)
+        // }
+
+        data.forEach((item, index) => {
+            let difference = 0
+            
+            console.log('totaldifference:' + totaldifference)
+            console.log(`index is ${index}`)
+            for (var x = 0; x < req.body.scores[x].length; x++) {
+                difference += Math.abs(data[index].scores[x] - parseInt(req.body.scores[x]))
+            }
+            if (difference < totaldifference) {
+                totaldifference = difference;
+                console.log(`if statement = index is ${index}`)
+                bestFriendIndex = index
+                console.log(`best friend ${bestFriendIndex}`)
+            }
+        })
+
         data.push(req.body)
+        res.json(data[bestFriendIndex])
     })
-    var bestFriendIndex = 0;
-    var minimumDifference = 40;
+
+
+
 }
 
 
