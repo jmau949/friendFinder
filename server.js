03fc833ef19cd80b19a7f154d33e6dee9ee70b08
Jonathan Mau <jmau949@gmail.com>
2019-09-17 21:01:36 -0500

    adding js

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ let app = express();
 let PORT = 8000
 let html = require('./app/routes/htmlRoutes')
 let api = require('./app/routes/apiRoutes');
+let path = require('path')
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/app/public')))
 
 
 html(app);
